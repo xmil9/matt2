6a3808db13c7a7e171ef48e6f23dc3c3f25ce101
Mike Lindner <mlindner9@yahoo.com>
2021-09-18 21:17:11 -0400

    Some tests for calculating the next move.

--- tests/game_tests.cpp
+++ tests/game_tests.cpp
@@ -36,7 +36,35 @@ void testPositionCtor()
       VERIFY(g.currentMoveIdx() == 0, caseLabel);
    }
 }
-   
+
+
+void testCalcNextMove()
+{
+   {
+      const std::string caseLabel =
+         "Game::calcNextMove in 1 turn for white with checkmate";
+
+      Game g{Position{"Kwa1 Kbd4 wc3"}};
+      const auto& nextPos = g.calcNextMove(Color::White, 1);
+
+      VERIFY(nextPos == Position{"Kwa1 wd4"}, caseLabel);
+      VERIFY(g.countMoves() == 1, caseLabel);
+      VERIFY(g.getMove(0) == Move(BasicMove{Relocation{Pw, c3, d4}, Kb}), caseLabel);
+   }
+   {
+      const std::string caseLabel =
+         "Game::calcNextMove in 1 turn for black with best move not the "
+         "one that takes the highest piece";
+
+      Game g{Position{"Kwb2 Bwg3 Nwf4 wb6 Kbe8 Qbd6"}};
+      const auto& nextPos = g.calcNextMove(Color::Black, 1);
+
+      VERIFY(nextPos == Position{"Kwb2 Bwg3 Nwf4 Kbe8 Qbb6"}, caseLabel);
+      VERIFY(g.countMoves() == 1, caseLabel);
+      VERIFY(g.getMove(0) == Move(BasicMove{Relocation{Qb, d6, b6}, Pw}), caseLabel);
+   }
+}
+
 } // namespace
 
 
@@ -46,4 +74,5 @@ void testGame()
 {
    testDefaultCtor();
    testPositionCtor();
+   testCalcNextMove();
 }
