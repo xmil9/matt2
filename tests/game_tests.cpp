29eddd05c2436f5b6c3b76908202dca7cd631131
Mike Lindner <mlindner9@yahoo.com>
2021-11-06 16:55:00 -0400

    More tests for calculating the next move.

--- tests/game_tests.cpp
+++ tests/game_tests.cpp
@@ -42,6 +42,17 @@ void testCalcNextMove()
 {
    {
       const std::string caseLabel =
+         "Game::calcNextMove in 1 turn for black when taking the highest piece is best";
+
+      Game g{Position{"Kwb1 Rwe4 Bwg4 Kbd8 Bbf5"}};
+      const auto& nextPos = g.calcNextMove(Color::Black, 1);
+
+      VERIFY(nextPos == Position{"Kwb1 Bwg4 Kbd8 Bbe4"}, caseLabel);
+      VERIFY(g.countMoves() == 1, caseLabel);
+      VERIFY(g.getMove(0) == Move(BasicMove{Relocation{Bb, f5, e4}, Rw}), caseLabel);
+   }
+   {
+      const std::string caseLabel =
          "Game::calcNextMove in 1 turn for white with checkmate";
 
       Game g{Position{"Kwa1 Kbd4 wc3"}};
@@ -56,6 +67,7 @@ void testCalcNextMove()
          "Game::calcNextMove in 1 turn for black with best move not the "
          "one that takes the highest piece";
 
+      // If black took the knight the black queen would be taken by the opponent.
       Game g{Position{"Kwb2 Bwg3 Nwf4 wb6 Kbe8 Qbd6"}};
       const auto& nextPos = g.calcNextMove(Color::Black, 1);
 
